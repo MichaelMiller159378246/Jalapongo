83b07b83fa00e5e944077bc0ef36db7034cbf427
Dalton0022 <dalton.lheureux@yahoo.com>
2015-03-06 16:02:48 -0500

    Update Player.java

--- Jalapongo/src/jalapongo/Player.java
+++ Jalapongo/src/jalapongo/Player.java
@@ -1,28 +1,36 @@
-package jalapongo;
-
 /**
  * @author Mike
  * @version 1.0
  * @created 02-Mar-2015 3:19:45 PM
  */
-public class Player /* extends Paddle Game GUI */ {
+public class Player{
 
-	private int health;
+	private int lives;
 	private String name;
 	private Paddle player;
 
-	public Player(){
-
+	public Player(int pos){
+		player = new Paddle(pos);
+	}
+	
+	public void setName(String name){
+		this.name = name;
+	}
+	
+	public String getName(){
+		return name;
+	}
+	
+	public void setLives(int lives){
+		this.lives = lives;
+	}
+	
+	public int getLives(){
+		return lives;
+	}
+	
+	public void scoredOn(){
+		lives =- 1;
 	}
 
-	public void finalize() throws Throwable {
-		super.finalize();
-	}
-	public void getLocation(){
-
-	}
-
-	public void setLocation(){
-
-	}
 }//end Player
