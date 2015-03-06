14436f776cbaa0a3af33d92ac31730873c171519
Dalton0022 <dalton.lheureux@yahoo.com>
2015-03-06 15:09:33 -0500

    Paddle Try #2

--- Jalapongo/src/Class Model/Paddle.java
+++ Jalapongo/src/Class Model/Paddle.java
@@ -11,38 +11,63 @@ public class Paddle {
 	private Line Paddle;
 	private int PaddleLocX;
 	private int PaddleLocY;
+	private int pos;
 
-	public Paddle(){
-
-	}
-
-	/**
-	 * 
-	 * @exception Throwable
-	 */
-	public void finalize()
-	  throws Throwable{
-
+	public Paddle(int pos){
+		if(pos == 1){
+			PaddleLocX = 10;
+			Paddle = new Line(PaddleLocX,325,PaddleLocX,375);
+			this.pos = pos;
+		}
+		if(pos == 2){
+			PaddleLocY = 690;
+			Paddle = new Line(325,PaddleLocY,375,PaddleLocY);
+			this.pos = pos;
+		}
+		if(pos == 3){
+			PaddleLocX = 690;
+			Paddle = new Line(PaddleLocX,325,PaddleLocX,375);
+			this.pos = pos;
+		}
+		if(pos == 4){
+			PaddleLocY = 10;
+			Paddle = new Line(325,PaddleLocY,375,PaddleLocY);
+			this.pos = pos;
+		}	
 	}
 
 	public int getLength(){
-		return 0;
+		int length = 0;
+		if(pos == 1 || pos == 3){
+			length = (int)(Paddle.getEndX() - Paddle.getStartX());
+		}else{
+			length = (int)(Paddle.getEndY() - Paddle.getStartY());
+		}
+		return length;
 	}
 
-	public int getXstart(){
-		return 0;
+	public void setLength(int length){
+		int start;
+		int end;
+		if(pos == 1 || pos == 3){
+			start = (int)Paddle.getStartX();
+			end = start + length;
+			Paddle.setEndX((double)end);
+		}
+		else{
+			start = (int)Paddle.getStartY();
+			end = start + length;
+			Paddle.setEndY((double)end);
+		}
 	}
 
-	public int getYstart(){
-		return 0;
-	}
-
-	public int setXstart(){
-		
-
-	}
-
-	public int setYstart(){
-
-	}
 }//end Paddle
+/** For Ball Class
+public int setXstart(){
+	return (int)Math.ceil(Math.random()*300+200);
+}
+
+public int setYstart(){
+	return (int)Math.ceil(Math.random()*300+200);
+}
+*/
